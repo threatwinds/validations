cebe9ea863a48763e91710aa5572e8501802ed62
Osmany Montero <osmontero@icloud.com>
2023-04-05 00:10:32 +0300

    convert to int64 and float64

--- float.go
+++ float.go
@@ -3,12 +3,17 @@ package validations
 import (
 	"fmt"
 	"net/http"
+	"reflect"
 
 	"github.com/quantfall/rerror"
 	"google.golang.org/grpc/codes"
 )
 
 func ValidateFloat(value interface{}) (float64, string, *rerror.Error) {
+	t := reflect.TypeOf(value)
+	if t.Kind() == reflect.Int64{
+		value = float64(value.(int64))
+	}
 	v, ok := value.(float64)
 	if !ok {
 		return 0, "", rerror.ErrorF(http.StatusBadRequest, codes.InvalidArgument, "value is not float: %v", value)
--- integer.go
+++ integer.go
@@ -3,15 +3,20 @@ package validations
 import (
 	"fmt"
 	"net/http"
+	"reflect"
 
 	"github.com/quantfall/rerror"
 	"google.golang.org/grpc/codes"
 )
 
 func ValidateInteger(value interface{}) (int64, string, *rerror.Error) {
-	v, ok := value.(float64)
+	t := reflect.TypeOf(value)
+	if t.Kind() == reflect.Float64 {
+		value = int64(value.(float64))
+	}
+	v, ok := value.(int64)
 	if !ok {
-		return 0, "", rerror.ErrorF(http.StatusBadRequest, codes.InvalidArgument, "value is not numeric: %v", value)
+		return 0, "", rerror.ErrorF(http.StatusBadRequest, codes.InvalidArgument, "value is not integer: %v", value)
 	}
 
 	return int64(v), GenerateSHA3256(fmt.Sprint(int(v))), nil
